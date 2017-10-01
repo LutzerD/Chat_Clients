862b89dabfe045ad7b6b575529bb4b067060605b
David Lutzer <LutzerD@users.noreply.github.com>
2017-10-01 12:39:14 -0600

    Clean(er) message output. New class for incoming server messages.

--- src/main/java/irc_main.java
+++ src/main/java/irc_main.java
@@ -1,3 +1,5 @@
+import com.sun.org.apache.bcel.internal.generic.SWITCH;
+
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.net.Socket;
@@ -23,6 +25,11 @@ public class irc_main {
         socketInput = new Scanner(s.getInputStream());
 
         //TODO: Include input validation -> alpha numeric
+        //TODO: Edit names such that nickname is always ok. i.e, make nickname alphabetica
+        //TODO: Make user class
+        //TODO: PGP encryption for the messages -> would be cool to learn
+        // the append random string to it, concatenated with character such ad ~. from
+        // there you may .split to recieve real name.
         System.out.println("What's good world");
         Scanner console = new Scanner(System.in);
 
@@ -30,12 +37,13 @@ public class irc_main {
         nick = console.nextLine();
 
         //TODO: Add random usernames / realnames, shits too much effort. random number from 1-26, matches with letter, gg.
-        System.out.println("Enter a username please");
-        username = console.nextLine();
+        //System.out.println("Enter a username please");
+        //username = console.nextLine();
+        username = "bootyButts";
 
-        System.out.println("Enter a realname please");
-        realName = console.nextLine();
-        //realName = "Peebles Jim";
+        //System.out.println("Enter a realname please");
+        //realName = console.nextLine();
+        realName = "Peebles Jim";
 
         chatRoom = "##ece-usask";
         System.out.println("Enter a desired chatroom. Default: " + chatRoom);
@@ -44,67 +52,119 @@ public class irc_main {
             chatRoomTemp = console.nextLine();
         }
 
-        System.out.println("Joining room.");
+        System.out.println("Loading room... (10s Please)");
 
         ircInput("NICK",nick);
         ircInput("USER",username + " 0 * :" + realName);
         ircInput("JOIN", chatRoom);
 
         boolean MOTDswitch = false;
-        String serverMessageTemp;
+        String[] serverMessageTemp;
+        String[] serverMessageList = new String[4];
+        String[] serverMessageListTemp;
 
+        String serverMessageType;
+        String incomingUser;
+        String incomingHostname;
         /*
         This is the where the chat input is collected via serverMessage.
         MOTDswitch is used so that the bulk of the original content is skipped, up until the room is joined.
-
         */
 
-        String serverMessage;
-        String targetMessage = "JOIN " + chatRoom;
-
+        serverMessage servermessage = new serverMessage(chatRoom);
         while (socketInput.hasNext()){
-            serverMessage = socketInput.nextLine();
-            System.out.println("<<<" + serverMessage);
+            serverMessage.setMessage(socketInput.nextLine());
+
+            serverMessageListTemp = new String[]{serverMessageList[1], serverMessageList[2], serverMessageList[3], serverMessage.getMessage()};
+            serverMessageList = serverMessageListTemp;
+
+
+
+            //This gotta stay in until im good :(
+            //Maybe keep track of last 4 server messages
+            //And print them when we r booted?
+            //System.out.println("<<<" + serverMessage.getMessage());
+
             /*
             This is placed before the MOTDswitch is flipped because the message it's flipped on is still unwanted.
             Once the switch is flipped, the input can be cleaned / ponged.
+            The main types of messages I will be getting afaik is the ping, privmsg and join(?)
+            This currently only deals with ping
              */
-            if(MOTDswitch == true) {
-                if (serverMessage.startsWith("PING")) {
-                    String spaceBuffer = " ";
-                    String pingContents = serverMessage.split(spaceBuffer, 2)[1];
-                    ircInput("PONG", pingContents);
-                    ircInput("LUSER"," ");
-                    ircInput("NAMES"," ");
 
-                }else{
-                    System.out.println("<<<" + serverMessage);
-                }
+             /*
+            Current returns of .checkServerMessage():
+            {
+            "Joined" : You have entered chatroom
+            "NickUsed" : Nickname is currently in use
+            "TimeOut" : chatroom is timedout (force booted?)
+            "MessageRecieved" : incoming message from other user
+            String pingcontents : The unique server-based string that is responded to so there is no timeout.
+            "n/a" : None of the above clicked, this is the default response
             }
-
-            /*
-            Once the keyword is reached, the switch (MOTDswitch) is flipped, and messages can continue to be cleaned and formatted.
-            This is wrapped in a try/catch because sometimes the server message length < targetmessage length, thus causing it to check
-            e.g. index(-2)
             */
-            try{
-                serverMessageTemp = serverMessage.substring(serverMessage.length() - targetMessage.length());
-                if (serverMessageTemp.equals(targetMessage) && MOTDswitch == false){
-                    MOTDswitch = true;
-                    System.out.println("Entered chat room " + chatRoom);
-                } }catch (Exception e){}
-        }
+            switch(serverMessage.checkServerMessage()){
+                case "Joined" : System.out.println("Joined room " + chatRoom); //Concern for joining multiple rooms?
+                    break;
+                case "NickUsed" : System.out.println("Nickname in use.");//Idk if this is a problem? since doesn't it just put @ infront of name.
+                                    closeConn(s,"stop", serverMessageList);
+                    break;
+                case "TimeOut" : closeConn(s,"stop", serverMessageList);
+                    break;
+                case "MessageRecieved" :
+                    //System.out.println("Message Recieved: " + serverMessage.getMessage());
+                    //Split at ! and take ! on? maybe format ip@ name$?
+                    serverMessageTemp = serverMessage.getMessage().split(":");
+                    incomingUser = serverMessageTemp[1].split("!",2)[0];
+                    incomingHostname = serverMessageTemp[1].split("freenode/ip.")[1].split(" ")[0];
+                    System.out.println(incomingHostname + "@ " + incomingUser + "$ " + serverMessageTemp[2]);
+                    break;
+                case "n/a" :
+                    break;
+                //Having the pong response as default is probably pretty sloppy programming but life is tough.
+                default: ircInput("PONG", serverMessage.checkServerMessage());
 
+            }
+        }
+        closeConn(s,"stop" , serverMessageList);
+
+
+    }
+
+        /*
+        Once the keyword is reached, the switch (MOTDswitch) is flipped, and messages can continue to be cleaned and formatted.
+        This is wrapped in a try/catch because sometimes the server message length < targetmessage length, thus causing it to check
+        e.g. index(-2)
+        */
+
+
+
+    private static void closeConn(Socket s, String directive, String[] serverMessageList) throws IOException {
         //Data cleanup upon completion
         socketInput.close();
         socketOutput.close();
         s.close();
+        switch(directive.toLowerCase()){
+            case "r":
+                break;
+            case "stop":
+                for (String sm : serverMessageList){
+                    System.out.println(sm);
+                }
+                System.exit(0);
+                break;
+        }
     }
 
+    /*
+    This function is used to communicate with the server.
+    Communication is done through the socket output.
+    Directive is typically the freenode command, unless it is just a message, then MSG is fine.
+     */
     private static void ircInput(String directive, String msg) {
 
         if (directive == "MSG"){
-            fullMessage = "PRIVMSG ##ece-usask " + msg;
+            fullMessage = "PRIVMSG " + chatRoom + msg;
         }else {
             fullMessage = directive +" "+ msg;
         }
---
+++ src/main/java/serverMessage.java
@@ -0,0 +1,79 @@
+public class serverMessage {
+    private static String serverMessage;
+    private static String chatRoom;
+    private static int serverMessageLength;
+
+    public serverMessage(String currentChatRoom){
+        chatRoom = currentChatRoom;
+    }
+
+    public static void setMessage(String m){
+        serverMessage = m;
+        serverMessageLength = serverMessage.length();
+    }
+
+    public static String getMessage(){
+        return serverMessage;
+    }
+
+
+    //TODO: IDea: this will return message type, then case/switch up top will determine whats next
+    /*
+    Current returns:
+    {
+    "Joined" : You have entered chatroom
+    "NickUsed" : Nickname is currently in use
+    "TimeOut" : chatroom is timedout (force booted?)
+    "MessageRecieved" : incoming message from other user
+    String pingcontents : The unique server-based string that is responded to so there is no timeout.
+    "n/a" : None of the above clicked, this is the default response
+    }
+     */
+    public static String checkServerMessage() {
+        String joinedResponse =  "JOIN " + chatRoom;
+        String nickInUseResponse = "Nickname is already in use.";
+        String timeOutResponse = "(Connection timed out)";
+        String serverMessageTemp;
+        String responseCode;
+
+        //Separate try catches? but try/catch latency is not optimal, :C
+        //TODO: Optimize for different message lengths
+
+        if(serverMessageLength - joinedResponse.length()>=0){
+            serverMessageTemp = serverMessage.substring(serverMessage.length() - joinedResponse.length());
+            if (serverMessageTemp.equals(joinedResponse)){
+                return "Joined";
+            }
+        }
+
+        //doesnt work
+        //TODO: loop when nick used
+        if(serverMessageLength - nickInUseResponse.length()>=0) {
+            serverMessageTemp = serverMessage.substring(serverMessage.length() - nickInUseResponse.length());
+            if (serverMessageTemp.equals(nickInUseResponse)) {
+                return "NickUsed";
+            }
+        }
+
+        if(serverMessageLength - timeOutResponse.length()>=0) {
+            serverMessageTemp = serverMessage.substring(serverMessage.length() - timeOutResponse.length());
+            if (serverMessageTemp.equals(timeOutResponse)) {
+                return "TimeOut";
+            }
+        }
+
+        if (serverMessage.startsWith("PING")) {
+            String spaceBuffer = " ";
+            String pingContents = serverMessage.split(spaceBuffer, 2)[1];
+            return pingContents;
+        }
+
+        String spaceBuffer = " ";
+        if (serverMessage.split(spaceBuffer,3)[1].equals("PRIVMSG")) {
+            return "MessageRecieved";
+        }
+
+
+        return "n/a";
+    }
+}
